70bc504377d354ccb25ae65a6d2c6154a1f4aac2
Tech-Gunner <146823477+Tech-Gunner@users.noreply.github.com>
2024-01-11 16:55:05 +0500

    / to jump to the search bar

--- Frontend/src/routes/assets/Header.svelte
+++ Frontend/src/routes/assets/Header.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		addEventListener("keyup", jump);
+	});
+	
+	function jump(e: KeyboardEvent): any {
+		const search = document.querySelector("#sea") as HTMLInputElement;
+
+		if (
+			e.key == "/" &&
+			!(document.activeElement instanceof HTMLInputElement) &&
+			!(document.activeElement instanceof HTMLTextAreaElement)
+		) {
+			search.focus();
+		}
+	}
+</script>
+
 <header>
 	<span class="logo"
 		><a data-sveltekit-preload-data href="/">
