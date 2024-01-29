f3efc5b8a1e2ea74b60da8a2961c0ff1e0e8a8bc
Tech-Gunner <146823477+Tech-Gunner@users.noreply.github.com>
2024-01-29 14:03:34 +0500

    Containerized the application using docker, Next step: migrate to Elysia JS

--- Backend/app.js
+++ Backend/app.js
@@ -28,7 +28,7 @@ app.use('/blog', blog)
 app.use('/profile', user)
 app.use('/auth', auth)
 
-const PORT = process.env.PORT ?? 5000 ?? 6000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`)
