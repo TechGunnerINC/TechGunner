b3e3aa4adf126238869732d59e95b87f8e6a4b2c
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-03-26 18:13:14 +0500

    Improved init.sh

--- init.sh
+++ init.sh
@@ -2,14 +2,31 @@
 
 echo "Initializing Frontend... ✨"
 cd frontend
-bun update
+bun i
 
 echo "Initializing Backend... 💻"
 cd ../backend
-bun update
+bun i
 
 echo "Launching Docker... 🐳"
-docker compose up -d
-docker compose watch
+retry=5
 
-exit 0
+command="docker compose watch"
+
+attempt=1
+while [[ $attempt -le $retry ]]; do
+  if $command; then
+    echo "Docker launched successfully!"
+    echo "🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳🐳"
+    break
+  else
+    echo "Error while launching Docker, retrying attempt $attempt/$retry..."
+    ((attempt++))
+  fi
+done
+
+if [[ $attempt -gt $retry ]]; then
+  echo "Docker failed to execute after $retry attempts."
+  exit 1
+fi
+docker compose logs -f
