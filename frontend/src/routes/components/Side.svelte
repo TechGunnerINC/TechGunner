a269a6c689baad77da6d1f204e447af1ece04eb0
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-03-26 11:26:30 +0500

    Simplified sidebar

--- frontend/src/routes/components/Side.svelte
+++ frontend/src/routes/components/Side.svelte
@@ -5,68 +5,79 @@
 
 	let show = false
 
-	function open() {
+	function toggleMenu() {
 		show = !show
 	}
 
 	onMount(() => {
 		addEventListener('keydown', (e: KeyboardEvent) => {
-			if ((e.ctrlKey && e.key == ' ') || (e.metaKey && e.key == ' ')) {
-				open()
+			if ((e.ctrlKey || e.metaKey) && e.key === ' ') {
+				toggleMenu()
 			}
-			if (e.key === 'Escape' && show === true) {
+			if (e.key === 'Escape' && show) {
 				show = false
 			}
 		})
 	})
 </script>
 
-{#if show}
-	<button class="bg" on:click={open} />
-{/if}
-
-<button on:click={open} class="material-symbols-outlined menu"> menu </button>
+<button on:click={toggleMenu} class="material-symbols-outlined menu" aria-label="Toggle menu">
+	menu
+</button>
 
 {#if show}
+	<button class="bg" on:click={toggleMenu} aria-label="Close menu"></button>
 	<nav transition:fade={{ delay: 50, duration: 100 }}>
-		{#each ['/', '/explore', '/messages', '/blogs', '/courses', '/freelance', '/tools', '/wallpapers', '/about', '/contact', '/projects'] as link}
-			<a class:act={$page.url.pathname === link} data-sveltekit-preload-data href={link}>
-				<b
-					class="ico fa-solid"
-					class:fa-house-chimney={link === '/'}
-					class:fa-magnifying-glass={link === '/explore'}
-					class:fa-envelope={link === '/messages'}
-					class:fa-pen={link === '/blogs'}
-					class:fa-user-graduate={link === '/courses'}
-					class:fa-briefcase={link === '/freelance'}
-					class:fa-wrench={link === '/tools'}
-					class:fa-image={link === '/wallpapers'}
-					class:fa-user={link === '/about'}
-					class:fa-comment={link === '/contact'}
-					class:fa-landmark={link === '/projects'} /><b class="link"
-					>{link === '/explore'
-						? 'Explore'
-						: link === '/messages'
-							? 'Messages'
-							: link === '/blogs'
-								? 'Blogs'
-								: link === '/courses'
-									? 'Courses and Tutorials'
-									: link === '/freelance'
-										? 'Freelance'
-										: link === '/tools'
-											? 'Tools'
-											: link === '/wallpapers'
-												? 'Wallpapers'
-												: link === '/about'
-													? 'About Me'
-													: link === '/contact'
-														? 'Contact Me'
-														: link === '/projects'
-															? 'Our Projects'
-															: 'Home'}</b>
-			</a>
-		{/each}
+		<a class:act={$page.url.pathname === '/'} data-sveltekit-preload-data href="/">
+			<b class="ico fa-solid fa-house-chimney"></b>
+			<b class="link">Home</b>
+		</a>
+		<a class:act={$page.url.pathname === '/explore'} data-sveltekit-preload-data href="/explore">
+			<b class="ico fa-solid fa-magnifying-glass"></b>
+			<b class="link">Explore</b>
+		</a>
+		<a class:act={$page.url.pathname === '/messages'} data-sveltekit-preload-data href="/messages">
+			<b class="ico fa-solid fa-envelope"></b>
+			<b class="link">Messages</b>
+		</a>
+		<a class:act={$page.url.pathname === '/blogs'} data-sveltekit-preload-data href="/blogs">
+			<b class="ico fa-solid fa-pen"></b>
+			<b class="link">Blogs</b>
+		</a>
+		<a class:act={$page.url.pathname === '/courses'} data-sveltekit-preload-data href="/courses">
+			<b class="ico fa-solid fa-user-graduate"></b>
+			<b class="link">Courses and Tutorials</b>
+		</a>
+		<a
+			class:act={$page.url.pathname === '/freelance'}
+			data-sveltekit-preload-data
+			href="/freelance">
+			<b class="ico fa-solid fa-briefcase"></b>
+			<b class="link">Freelance</b>
+		</a>
+		<a class:act={$page.url.pathname === '/tools'} data-sveltekit-preload-data href="/tools">
+			<b class="ico fa-solid fa-wrench"></b>
+			<b class="link">Tools</b>
+		</a>
+		<a
+			class:act={$page.url.pathname === '/wallpapers'}
+			data-sveltekit-preload-data
+			href="/wallpapers">
+			<b class="ico fa-solid fa-image"></b>
+			<b class="link">Wallpapers</b>
+		</a>
+		<a class:act={$page.url.pathname === '/about'} data-sveltekit-preload-data href="/about">
+			<b class="ico fa-solid fa-user"></b>
+			<b class="link">About Me</b>
+		</a>
+		<a class:act={$page.url.pathname === '/contact'} data-sveltekit-preload-data href="/contact">
+			<b class="ico fa-solid fa-comment"></b>
+			<b class="link">Contact Me</b>
+		</a>
+		<a class:act={$page.url.pathname === '/projects'} data-sveltekit-preload-data href="/projects">
+			<b class="ico fa-solid fa-landmark"></b>
+			<b class="link">Our Projects</b>
+		</a>
 	</nav>
 {/if}
 
